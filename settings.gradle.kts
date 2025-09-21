58dafdeb58d8794a408da1dc782e01088a01e55b
gabrielesp2025-arch <gabrielesp2025@gmail.com>
2025-09-21 02:32:44 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,14 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.6.0"
+        id("org.jetbrains.kotlin.android") version "2.0.20"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -12,5 +19,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "TallerGK25"
 include(":app")
